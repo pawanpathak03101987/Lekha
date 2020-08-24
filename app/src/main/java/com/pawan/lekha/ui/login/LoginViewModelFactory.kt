3114ad9a8003b5b4b2979c3c4363c4d4c95c6e1b
pawanpathak03101987 <pawanpathak03.10.1987@gmail.com>
2020-08-24 10:43:51 +0530

    Initial commit

---
+++ app/src/main/java/com/pawan/lekha/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.pawan.lekha.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.pawan.lekha.data.LoginDataSource
+import com.pawan.lekha.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
